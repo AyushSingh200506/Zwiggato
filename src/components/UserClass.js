a9a866964d1400b438ccd3a55f04fdb7c0b91073
AyushSingh200506 <ayushsingh200506@gmail.com>
2026-06-10 02:02:11 +0530

    Add count state to User and UserClass components for displaying counts

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,18 @@ import React from 'react';
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
     render() {
         const { name, location, contact } = this.props;
+        const { count, count2 } = this.state;
         return (
             <div className="user-card">
+                <h2>Count: {count}</h2>
+                <h2>Count2: {count2}</h2>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h3>Contact: {contact}</h3>
