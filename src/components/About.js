538ec31cd43ed940c2ab5e16a39285e80a6d4c01
AyushSingh200506 <ayushsingh200506@gmail.com>
2026-06-10 01:51:12 +0530

    Add User and UserClass components with styling for user cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About Us Page</h1>
             <h2>This is the About Us page.</h2>
+            <User name="Ayush Singh" location="Gorakhpur" contact="9580786562" />
+            <UserClass name="Ritesh Gurkhe" location="Mumbai" contact="9876543210" />
         </div>
     );
 }
